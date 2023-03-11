56a3e45e81afda3804b6ca43f1103cac0ad7a3e7
THEBEASTCODES <saksham.2022ug3028@iiitranchi.ac.in>
2023-03-11 16:02:03 +0530

    sfahh

---
+++ web/main.js
@@ -0,0 +1,16 @@
+window.addEventListener('load',()=>{
+const form = document;
+querySelector('#task-id')
+
+const input= document;
+querySelector
+("#new-input-task")
+console.log(input);
+const tasks_List= document.querySelector('.task');
+console.log(tasks_List);
+})
+
+form.addEventListener('submit', ()=>{
+    console.log(input.value);
+
+})
